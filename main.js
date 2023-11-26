c791435f8474900a204b39099d60935a26a78f5c
Nuruxs <johnf.git@gmail.com>
2023-11-26 17:54:57 -0300

    testeziito

--- main.js
+++ main.js
@@ -4,16 +4,22 @@ const app = express()
 const banco = require('./database/bd').conexao
 const cors = require('cors')
 
-app.use(cors())
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use((req, res) => {
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader(
       "Access-Control-Allow-Methods",
       "PUT, POST, GET, DELETE, PATCH, OPTIONS"
     );
+    next();
 });
+
+app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 
 app.get('', (req, res) => {
     banco.getConnection((erro, cnx) => {
